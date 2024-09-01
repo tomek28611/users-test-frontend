2ffbfe8559c41f2ee2a0f0788fb88e7c017b26f7
Tom <tomek286@yahoo.com>
2024-09-01 05:57:35 +0200

    commit

---
+++ src/layout/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-light navbar-dark bg-primary">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <Link className='btn btn-outline-light' to="/adduser">Add User</Link>
+            </nav>
+        </div>
+    )
+}
